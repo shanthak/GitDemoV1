e77efb1c0422ba8fd30dfaa5b355f29c105e8071
IBM <IBM@DESKTOP-S0IEADH>
2020-03-27 18:59:24 +0530

    added additional statement

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("This is a Git  test 1");
 		System.out.println("This is a Git  test 2");
 
 	}
